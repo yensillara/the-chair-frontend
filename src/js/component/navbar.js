0ac22ef0e3e9d94cf7c6cb349c18b58d4992a5a3
mariejosea <marie1danez@gmail.com>
2021-02-10 17:28:37 -0400

    Arreglar el problema de bootstrap

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import blackBrand from "../../img/black-brand.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<img width="200" height="50" src={blackBrand} />
+				</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+
+			<button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-primary">
+				sign out
+			</button>
+
+			<div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">
+								Modal title
+							</h5>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+						</div>
+						<div className="modal-body">...</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+								Close
+							</button>
+							<button type="button" className="btn btn-primary">
+								Save changes
+							</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import professionalUser from "../../img/vista1.jpg";
+import homeUser from "../../img/vista2.jpg";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+	<>
+		<div className="home-user text-left">
+			<img className="image1" src={homeUser} />
+		</div>
+		<div className="professional-user text-right">
+			<img src={professionalUser} className="image2" />
+		</div>
+	</>
 );
